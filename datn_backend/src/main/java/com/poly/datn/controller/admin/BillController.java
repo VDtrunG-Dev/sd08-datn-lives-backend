21e73c5f126656e242aeb9888a77cc98ed44bea1
dangquangduy162 <106823937+dangquangduy162@users.noreply.github.com>
2023-10-25 02:02:18 +0700

    Update BillController.java

--- datn_backend/src/main/java/com/poly/datn/controller/admin/BillController.java
+++ datn_backend/src/main/java/com/poly/datn/controller/admin/BillController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
-@RequestMapping
+@RequestMapping("/api/bill")
 public class BillController {
 
 }
