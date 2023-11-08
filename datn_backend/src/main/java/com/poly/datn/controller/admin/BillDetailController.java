d9fcd281c966521456fb634c04f9804360c54835
quangha1103 <quanghaboom@gmail.com>
2023-11-08 22:11:51 +0700

    commit config vnpay demo message error

--- datn_backend/src/main/java/com/poly/datn/controller/admin/BillDetailController.java
+++ datn_backend/src/main/java/com/poly/datn/controller/admin/BillDetailController.java
@@ -5,6 +5,8 @@ import com.poly.datn.dto.ResponseObject;
 import com.poly.datn.model.TBillDetail;
 import com.poly.datn.service.BillDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -101,5 +103,77 @@ public class BillDetailController {
                     new ResponseObject("failed", "Can't find any to delete", "")
             );
         }
+    }
+
+    @GetMapping("/da-thanh-toan")
+    public ResponseEntity<ResponseObject> daThanhToanPage(@RequestParam(defaultValue = "0", name = "pageNo") Integer pageNo
+            , @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Page Da thanh toan", billDetailService.PageGetAllBillDetailsDaThanhToan(pageable))
+        );
+    }
+
+    @GetMapping("/da-thanh-toan")
+    public ResponseEntity<ResponseObject> daHuyPage(@RequestParam(defaultValue = "0", name = "pageNo") Integer pageNo
+            , @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Page Da thanh toan", billDetailService.PageGetAllBillDetailsDaHuy(pageable))
+        );
+    }
+
+    @GetMapping("/da-thanh-toan")
+    public ResponseEntity<ResponseObject> dangVanChuyenPage(@RequestParam(defaultValue = "0", name = "pageNo") Integer pageNo
+            , @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Page Da thanh toan", billDetailService.PageGetAllBillDetailsDaThanhToan(pageable))
+        );
+    }
+
+    @GetMapping("/da-thanh-toan")
+    public ResponseEntity<ResponseObject> daVanChuyenPage(@RequestParam(defaultValue = "0", name = "pageNo") Integer pageNo
+            , @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Page Da thanh toan", billDetailService.PageGetAllBillDetailsVanChuyen(pageable))
+        );
+    }
+
+    @GetMapping("/da-thanh-toan")
+    public ResponseEntity<ResponseObject> dangHoanPage(@RequestParam(defaultValue = "0", name = "pageNo") Integer pageNo
+            , @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Page Da thanh toan", billDetailService.PageGetAllBillDetailsDangHoan(pageable))
+        );
+    }
+
+    @GetMapping("/da-thanh-toan")
+    public ResponseEntity<ResponseObject> daHoanKiemTraPage(@RequestParam(defaultValue = "0", name = "pageNo") Integer pageNo
+            , @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Page Da thanh toan", billDetailService.PageGetAllBillDetailsDaHoanKiemTra(pageable))
+        );
+    }
+
+    @GetMapping("/da-thanh-toan")
+    public ResponseEntity<ResponseObject> daTraHoanTienPage(@RequestParam(defaultValue = "0", name = "pageNo") Integer pageNo
+            , @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Page Da thanh toan", billDetailService.PageGetAllBillDetailsDaTraHoanTien(pageable))
+        );
+    }
+
+    @GetMapping("/da-thanh-toan")
+    public ResponseEntity<ResponseObject> daDoiTraPage(@RequestParam(defaultValue = "0", name = "pageNo") Integer pageNo
+            , @RequestParam(defaultValue = "10", name = "pageSize") Integer pageSize){
+        Pageable pageable= PageRequest.of(pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Page Da thanh toan", billDetailService.PageGetAllBillDetailsDoiTra(pageable))
+        );
     }
 }
