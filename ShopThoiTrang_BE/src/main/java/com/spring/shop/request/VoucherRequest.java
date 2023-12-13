103b04a790b5edea29ab7e494e3cccf9dc226b57
Nguyễn Văn Đạt <nguyenvandathn963@gmail.com>
2023-12-13 21:04:24 +0700

    validate

--- ShopThoiTrang_BE/src/main/java/com/spring/shop/request/VoucherRequest.java
+++ ShopThoiTrang_BE/src/main/java/com/spring/shop/request/VoucherRequest.java
@@ -1,5 +1,6 @@
 package com.spring.shop.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,14 +10,32 @@ import java.util.Date;
 @Getter
 @Setter
 public class VoucherRequest {
+
+    @NotBlank(message = "Mã không được bỏ trống !")
     private String Code;
+
+    @NotBlank(message = "Tên không được bỏ trống !")
     private String Name;
+
+
     private Boolean TypeVoucher;
+
+
     private Boolean IsVoucher;
+
+
     private Integer Discount;
+
+
     private BigDecimal Cash;
+
+
     private Date StartDate;
+
+
     private Date EndDate;
+
+
     private Integer Minimum;
 
 }
