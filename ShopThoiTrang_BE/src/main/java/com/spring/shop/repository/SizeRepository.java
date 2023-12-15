bdb7b4a76ad2d8e2f77814ce43def6dd00a79473
dangquangduy162 <106823937+dangquangduy162@users.noreply.github.com>
2023-12-15 13:34:45 +0700

    ngừng hđ siz

--- ShopThoiTrang_BE/src/main/java/com/spring/shop/repository/SizeRepository.java
+++ ShopThoiTrang_BE/src/main/java/com/spring/shop/repository/SizeRepository.java
@@ -1,7 +1,5 @@
 package com.spring.shop.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,12 +7,19 @@ import org.springframework.stereotype.Repository;
 
 import com.spring.shop.entity.Size;
 
+import java.util.List;
+
 @Repository
-public interface SizeRepository extends JpaRepository<Size,Integer> {
-    @Query(value = "Select e from Size e  order by e.CreateDate desc ")
+public interface SizeRepository extends JpaRepository<Size, Integer> {
+    @Query(value = "Select e from Size e order by e.CreateDate desc")
     public List<Size> getAll();
+
+    @Query(value = "Select e from Size e where e.Status = 1 order by e.CreateDate desc")
+    public List<Size> getStopWorking();
+
     @Query(value = "Select e from Size e where e.Name like :name")
     public List<Size> searchByName(@Param("name") String name);
+
     @Query(value = "select e from Size e where e.Id = :id")
-    public Size getById(@Param("id") Integer Id);
+    public Size getById(@Param("id") Integer id);
 }
--- ShopThoiTrang_BE/src/main/java/com/spring/shop/rest/SizeRest.java
+++ ShopThoiTrang_BE/src/main/java/com/spring/shop/rest/SizeRest.java
@@ -6,14 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.spring.shop.request.SizeRequest;
 import com.spring.shop.service.SizeService;
@@ -32,14 +25,21 @@ public class SizeRest {
         return ResponseEntity.ok(service.getAll());
     }
 
+    @GetMapping("/stopworking")
+    public ResponseEntity<?> findSizeStopWorking(){
+        return ResponseEntity.ok(service.getStopWorking());
+    }
+
     @GetMapping("/search/{name}")
-    public ResponseEntity<?> getAllByProductName(@PathVariable("name") String name){
+    public ResponseEntity<?> getAllBySizeName(@PathVariable("name") String name){
         return ResponseEntity.ok(service.getAllbyName(name));
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable("id") Integer id){
         return ResponseEntity.ok(service.getById(id));
     }
+
     @PostMapping()
     public ResponseEntity<?> add(@Valid @RequestBody SizeRequest request, BindingResult result){
         if (result.hasErrors()){
@@ -48,6 +48,7 @@ public class SizeRest {
         }
         return ResponseEntity.ok(service.add(request));
     }
+
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") Integer Id,@Valid @RequestBody SizeRequest request, BindingResult result){
         if (result.hasErrors()){
@@ -56,8 +57,18 @@ public class SizeRest {
         }
         return ResponseEntity.ok(service.update(Id,request));
     }
+
     @PutMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Integer Id){
         return ResponseEntity.ok(service.delete(Id));
+    }
+
+    @PutMapping("/restore/{id}")
+    public ResponseEntity<?> restore(@PathVariable("id") Integer Id){
+        return ResponseEntity.ok(service.restore(Id));
+    }
+    @PutMapping("/deletefake/{id}")
+    public ResponseEntity<?> deleteFake(@PathVariable("id") Integer Id){
+        return ResponseEntity.ok(service.deleteFake(Id));
     }
 }
